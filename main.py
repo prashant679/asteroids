8b838f3037ceddb93c503f0eaa40995e070fe024
prashant679 <prashantmail1808@gmail.com>
2026-07-09 21:26:10 +0530

    Add Player Sprit in the game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
@@ -11,14 +12,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     #Gameloop
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
