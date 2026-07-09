db5bfc8e0e3bddd45bf019c9a1655488869d547a
prashant679 <prashantmail1808@gmail.com>
2026-07-09 20:58:41 +0530

    Added Checking delta time

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0.0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_HEIGHT))
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    #Gameloop
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +20,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
