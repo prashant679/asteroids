6f425be290c57183b329e2e1b1db01e428fc8cb2
prashant679 <prashantmail1808@gmail.com>
2026-07-10 23:30:07 +0530

    Added Bullett in game

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from circleshape import CircleShape
+from shot import Shot
 import sys
 
 def main():
@@ -20,11 +21,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable,drawable)
     Asteroid.containers = (asteroids,updatable,drawable)
     AsteroidField.containers = updatable
-
+    Shot.containers = (shots,updatable,drawable)
         
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     asteriod_field = AsteroidField()
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH,PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH,PLAYER_TURN_SPEED, PLAYER_SPEED,PLAYER_SHOOT_SPEED
 import pygame
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self,x,y):
@@ -19,6 +20,15 @@ class Player(CircleShape):
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
 
+    def shoot(self):
+        bullet = Shot(self.position.x,self.position.y)
+        unit_vector = pygame.Vector2(0,1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SHOOT_SPEED
+        bullet.velocity += rotated_with_speed_vector
+
+
+
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
@@ -30,6 +40,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
         
         # in the Player class
@@ -40,3 +52,6 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+
+        
