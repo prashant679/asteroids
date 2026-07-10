ec32694fc60c46260e42ee738d3ccff2680cef76
prashant679 <prashantmail1808@gmail.com>
2026-07-10 12:19:18 +0530

    Added Asteroid spawing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -15,9 +17,15 @@ def main():
    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable,drawable)
-    
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = updatable
+
+        
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteriod_field = AsteroidField()
 
     
     #Gameloop
